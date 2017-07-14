bb38df4cb3f4f54ce96d0436d9ea2e4687fadc76
Rubén de Celis Hernández <rubendecelis@ingran.es>
2017-07-14 10:20:39 +0200

    Fixed headers error

--- crawler/crawler/spiders/browser_stats.py
+++ crawler/crawler/spiders/browser_stats.py
@@ -8,9 +8,18 @@ class BrowserStatsSpider(scrapy.Spider):
     start_urls = ['https://www.w3schools.com/Browsers/default.asp']
 
     def parse(self, response):
-        for table in response.css('table'):             
-            yield{
-                'name': 'Browsers Stats ' + table.css("th::text").extract_first(),
-                'header': table.css("th::text").extract(),
-                'data': table.css("td::text").extract()
-            }
+        first_table = 0
+        for table in response.css('table'):
+            if first_table == 0:
+                yield{
+                    'name': 'Browsers Stats ' + table.css("th::text").extract_first(),
+                    'header': [table.css("th::text").extract_first()] + table.css("th a::text").extract(),
+                    'data': table.css("td::text").extract()
+                }
+                first_table = 1
+            else:
+                yield{
+                    'name': 'Browsers Stats ' + table.css("th::text").extract_first(),
+                    'header': table.css("th::text").extract(),
+                    'data': table.css("td::text").extract()
+                }
